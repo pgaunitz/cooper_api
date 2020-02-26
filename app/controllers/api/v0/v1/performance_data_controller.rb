b48ceee17ceb60ee5daa45dad027bf6c0803226b
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-26 10:30:56 +0100

    added performance controller

---
+++ app/controllers/api/v0/v1/performance_data_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PerformanceDataController < ApplicationController
+    def create
+      data = PerformanceData.new(performance_data_params)
+  
+      if data.save
+        head :ok
+      else
+        render json: { error: data.errors.full_messages }
+      end
+    end
+  
+    private
+  
+    def performance_data_params
+      params.require(:performance_data).permit!
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
+      resources :performance_data, only: [:create]
+    
     end
   end
 end
